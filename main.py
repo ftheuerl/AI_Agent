5a19f790bc1b55141dfd5751820e83ab9eec858f
merendar <deepmind05@gmail.com>
2026-02-06 14:45:36 +0000

    Token usage added

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import os
+
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
 
 def main():
-    print("Hello from ai-agent!")
-    if api_key == None:
-        raise RuntimeError("environment variable not found")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("Gemini_API_KEY environment variable not set")
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    usage_metadata = response.usage_metadata
+    if usage_metadata is None:
+        raise RuntimeError("API request failed")
+#    user_prompt = contents
+    prompt_tokens = usage_metadata.prompt_token_count
+    response_tokens = usage_metadata.candidates_token_count
+
+#    print(user_prompt)
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
