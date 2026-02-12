b1b0d73eac2f356e6d8ea29c1fed8d6885718804
merendar <deepmind05@gmail.com>
2026-02-12 09:46:37 +0000

    enable AI to use single functions -no loop

--- call_function.py
+++ call_function.py
@@ -1,7 +1,7 @@
 from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.run_python_file import run_python_file, schema_run_python_file
-from functions.write_file import write_file, schema_write_file
+from functions.write_files_content import write_file, schema_write_file
 
 
 from google.genai import types
@@ -53,3 +53,4 @@ def call_function(function_call, verbose=False):
         )
     ],
 )
+
--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import os
 import argparse
 from prompts import system_prompt
 from functions.get_files_info import schema_get_files_info, schema_get_files_info
-from functions.call_function import available_functions
+from call_function import available_functions, call_function
 
 from dotenv import load_dotenv
 from google import genai
-#new add to start creating a history of messages
 from google.genai import types
 
 
@@ -15,11 +14,8 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-# Now we can access `args.user_prompt`
 
-# new add to create memory of messages
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -30,9 +26,6 @@ def main():
     response = client.models.generate_content(
     model='gemini-2.5-flash',
     contents=messages,
-    #config=types.GenerateContentConfig(
-    #system_instruction=system_prompt,
-    #temperature=0),
     config=types.GenerateContentConfig(
     tools=[available_functions], system_instruction=system_prompt #new add
         )
@@ -45,13 +38,23 @@ def main():
     prompt_tokens = usage_metadata.prompt_token_count
     response_tokens = usage_metadata.candidates_token_count
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
+#        print(f"User prompt: {args.user_prompt}")
+#        print(f"Prompt tokens: {prompt_tokens}")
+#        print(f"Response tokens: {response_tokens}")
+    function_results = []
     if response.function_calls:
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, args.verbose)
+            if not function_call_result.parts:
+                raise Exception('Error: parts returned as None')
+            f_response = function_call_result.parts[0].function_response
+            if f_response is None:
+                raise Exception('Error: No FunctionResponse object')
+            if f_response.response is None:
+                raise Exception('Error: No function result')
+            function_results.append(function_call_result.parts[0])
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print("Response:")
         print(response.text)
